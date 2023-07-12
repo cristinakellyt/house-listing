7c5b2b626408a0c7c03bb11b5c56b101c7989379
cristinakellyt <cristinakellyt@gmail.com>
2023-07-12 14:43:16 +0200

    refactor: delete default boilerplate code provide by vue-cli
    Also, inital router setup was added

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      redirect: '/houses'
+    },
+    {
+      path: '/houses',
+      component: null,
+      children: [
+        {
+          path: 'search',
+          component: null
+        },
+        {
+          path: 'create',
+          component: null
+        },
+        {
+          path: ':id',
+          component: null,
+          children: [
+            {
+              path: 'edit',
+              component: null
+            }
+          ]
+        }
+      ]
     },
     {
       path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      component: null
+    },
+    { path: '/:notFound(.*)', component: null }
   ]
 })
 
