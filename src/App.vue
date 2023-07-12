1a035c089cc976f97ce99cb1f3a9704f30d91ab6
cristinakellyt <cristinakellyt@gmail.com>
2023-07-12 23:37:09 +0200

    feat(header): add mobile view

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <TheHeader />
+  <TheHeader :windowWidth="windowWidth" />
   <base-width>
     <router-view></router-view>
   </base-width>
 </template>
 
 <script setup>
+import { onMounted, ref } from 'vue'
 import TheHeader from './components/layout/TheHeader.vue'
 import BaseWidth from './components/ui/BaseWidth.vue'
+
+const windowWidth = ref(0)
+
+onMounted(() => {
+  window.addEventListener('resize', handlerWindowResize)
+  handlerWindowResize()
+})
+
+const handlerWindowResize = () => {
+  windowWidth.value = window.innerWidth
+}
 </script>
--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -1,16 +1,44 @@
 <template>
   <header>
     <nav>
-      <router-link to="/houses"
+      <router-link to="/houses" v-if="desktopView"
         ><img src="./../icons/img_logo_dtt@3x.png" alt="dtt-logo"
       /></router-link>
       <ul>
-        <li><router-link to="/houses">Houses</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
+        <li>
+          <router-link to="/houses">
+            <span v-if="desktopView">Houses</span>
+            <img
+              v-else
+              id="mobile-header-home-btn"
+              src="../icons/ic_mobile_navigarion_home@3x.png"
+              alt="home page"
+            />
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/about">
+            <span v-if="desktopView">About</span>
+            <img
+              v-else
+              id="mobile-header-info-btn"
+              src="../icons/ic_mobile_navigarion_info@3x.png"
+              alt="about page"
+            />
+          </router-link>
+        </li>
       </ul>
     </nav>
   </header>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps(['windowWidth'])
+
+const desktopView = computed(() => props.windowWidth > 550)
+</script>
 
 <style scoped>
 header {
@@ -57,5 +85,38 @@ a:hover,
 a.router-link-active {
   font-weight: 700;
   color: var(--text-color-primary);
+}
+
+/* Header mobile view starts at 550px */
+@media only screen and (max-width: 34.375em) {
+  header {
+    bottom: 0;
+  }
+
+  header nav {
+    display: block;
+    padding: 0 calc((60 / 16) * 1rem);
+  }
+
+  img {
+    width: calc((30 / 16) * 1rem);
+    height: calc((30 / 16) * 1rem);
+  }
+
+  ul {
+    justify-content: space-between;
+  }
+
+  ul li {
+    width: fit-content;
+  }
+
+  a.router-link-active #mobile-header-info-btn {
+    content: url(./../icons/ic_mobile_navigarion_info_active@3x.png);
+  }
+
+  a.router-link-active #mobile-header-home-btn {
+    content: url(./../icons/ic_mobile_navigarion_home_active@3x.png);
+  }
 }
 </style>
